4dc6194416b3380aad7f7dd7528877f920174d22
SD Vinh <saddomc224@gmail.com>
2024-10-06 20:10:08 +0700

    Import 3

---
+++ Exercises/Mo ta thuat toan co cau truc dieu kien/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so diem cua hoc vien: ");
+        int diem = int.Parse(Console.ReadLine());
+
+        if (0<= diem && diem <=100)
+            {
+                if ( 35>diem)
+            {
+                Console.WriteLine("Loại E");
+            }
+            else if ( 35<=diem && diem <45)
+            {
+                Console.WriteLine("Loại D");
+            }
+            else if ( 45<=diem && diem <60)
+            {
+                Console.WriteLine("Loại C");
+            }
+            else if ( 60<=diem && diem <75)
+            {
+                Console.WriteLine("Loại B");
+            }
+            else if ( diem >= 75)
+            {
+                Console.WriteLine("Loại A");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nhap lai diem cua hoc vien");
+        }
+    }
+}
