76247c9f54324dd59a39f7c9672833e73a2fed96
SD Vinh <saddomc224@gmail.com>
2024-10-06 19:01:48 +0700

    Import 2

---
+++ Exercises/Mo ta thuat toan tinh diem trung binh/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Nhap diem toan:");
+        float toan = float.Parse(Console.ReadLine());
+        Console.Write("Nhap diem ly:");
+        float ly = float.Parse(Console.ReadLine());
+        Console.Write("Nhap diem hoa:");
+        float hoa = float.Parse(Console.ReadLine());
+
+        float average = (toan+ly+hoa)/3;
+        Console.WriteLine("Diem trung binh 3 mon: Toan, Ly, Hoa: {0:0.0}", average);
+    }
+}
---
+++ Practice/Thuat toan game doan so/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap mot so tu 0 den 9: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        Random random= new Random();
+        int M = random.Next(0,9);
+
+        if (0<= N && N<=9)
+        {
+            if (N==M)
+            {
+                Console.WriteLine("Dự đoán đúng");
+            }
+            else
+            {
+                Console.WriteLine("Dự đoán sai");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Vui long nhap lai so tu 0 den 9");
+        }
+    }
+}
