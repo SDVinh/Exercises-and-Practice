6817cc443e0cb9f327271dc3cd5f02f7a8f0b195
SD Vinh <saddomc224@gmail.com>
2024-10-06 18:38:41 +0700

    Import

---
+++ Practice/Thuat toan chuyen doi nhiet do/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap nhiet do C: ");
+        float C = float.Parse(Console.ReadLine());
+        float F = (9*C)/5+32;
+        Console.WriteLine(F);
+    }
+}
