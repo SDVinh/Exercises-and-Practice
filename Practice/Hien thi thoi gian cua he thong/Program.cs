a5c43fca47ad8b2649669cb0df8e055b5afd8a52
SD Vinh <saddomc224@gmail.com>
2024-10-19 20:03:43 +0700

    UPD

---
+++ Practice/Hien thi thoi gian cua he thong/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime locaDate = DateTime.Now;
+        Console.WriteLine("Date time now is: {0}", locaDate);
+    }
+}
